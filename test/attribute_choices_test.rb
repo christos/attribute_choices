338ce436c823087587153d128bf95ef0992d4e44
Christos Zisopoulos <christos@42linesofcode.com>
2009-05-07 02:14:45 +0200

    Bring the (few) tests up to date

--- test/attribute_choices_test.rb
+++ test/attribute_choices_test.rb
@@ -1,29 +1,29 @@
 require 'test_helper'
 
 
-class AttributesWithChoicesTest < ActiveSupport::TestCase
+class AttributeChoicesTest < ActiveSupport::TestCase
 
   class Profile < ActiveRecord::Base
     GENDER_CHOICES = {'m' => 'Man', 'f' => 'Woman'}
-    attributes_with_choices :gender => GENDER_CHOICES
+    attribute_choices :gender, GENDER_CHOICES
   end
   
   class ChildProfile < Profile
     GENDER_CHOICES = {'m' => 'Man', 'f' => 'Woman', 'o' => 'Other'}
-    attributes_with_choices :gender => GENDER_CHOICES
+    attribute_choices :gender , GENDER_CHOICES
   end
     
     
   test "It should add a class method to all ActiveRecord objects" do
-    assert_respond_to(Profile, :attributes_with_choices)
+    assert_respond_to(Profile, :attribute_choices)
   end
   
   test "An child AR object should not share the parent's chocies" do
     @parent = Profile.new(:gender => 'm')
     @parent2 = Profile.new(:gender => 'f')
     @child = ChildProfile.new(:gender => 'o')
-    puts @parent.gender(:display)
-    puts @parent2.gender(:display)
-    puts @child.gender(:display)
+    puts @parent.gender_display
+    puts @parent2.gender_display
+    puts @child.gender_display
   end
 end
