98d5076b8674313580d4a7e180f8a85453bb8b28
Christos Zisopoulos <christos@42linesofcode.com>
2009-05-11 18:22:57 +0200

    Test that attribute_choices are updateable at any time

--- test/attribute_choices_test.rb
+++ test/attribute_choices_test.rb
@@ -5,8 +5,9 @@ class AttributeChoicesTest < ActiveSupport::TestCase
 
   class Profile < ActiveRecord::Base
     attribute_choices :gender, {'m' => 'Man', 'f' => 'Woman'}
+    attribute_choices :salutation, [["mr", 'Mister'], ["mrs", 'Misses']]
   end
-  
+
   class ChildProfile < Profile
     attribute_choices :gender , {'m' => 'Man', 'f' => 'Woman', 'o' => 'Other'}
     attribute_choices :salutation, [
@@ -15,18 +16,28 @@ class AttributeChoicesTest < ActiveSupport::TestCase
       ["ms", 'Miss'],
     ]
   end
-    
-    
+
+
   test "It should add a class method to all ActiveRecord objects" do
     assert_respond_to(Profile, :attribute_choices)
   end
-  
+
   test "An child AR object should not share the parent's chocies" do
     assert_not_equal ChildProfile.gender_choices, Profile.gender_choices
   end
-  
+
   test "It should return nil as the display value for an attribute that isn't mapped" do
     @profile = ChildProfile.new(:salutation => 'master')
     assert_nil @profile.salutation_display
   end
+
+  test "Multiple calls to attribute_choices update the attribute choices" do    
+    assert_equal Profile.salutation_choices, [['Mister', 'mr'], ['Misses', 'mrs']]
+
+    class Profile < ActiveRecord::Base
+      attribute_choices :gender, [["mr", 'Señor'], ["mrs", 'Señora']]
+    end
+    assert_equal Profile.gender_choices, [['Señor', 'mr'], ['Señora', 'mrs']]
+  end
+
 end
